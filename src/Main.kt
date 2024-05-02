b011c5af28c6b718e2e7712d66d932036047394a
Hamza9052 <hamzh9052@gmail.com>
2024-05-02 14:39:14 +0100

    Problem Solve

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+fun main() {
+
+    multip(2)
+
+}
+fun multip (Number:Int):String =  if(Number % 2 == 0)  "Even" else  "Odd"
